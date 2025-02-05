8be52d822a124c7ff3da970da6a87e849752328f
kaung-san-hein <kaungsanhein2019@gmail.com>
2025-02-05 20:01:47 +0630

    add signup api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -6,6 +14,11 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post('signup')
+  async signup(@Body() body: any) {
+    return await this.usersService.signup(body);
+  }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
